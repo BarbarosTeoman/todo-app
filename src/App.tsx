c5b64cd77ab65ea3ea2feda1fc38f4557cab4b0f
Barbaros Teoman Kosoglu <btkkteoman@icloud.com>
2023-07-23 03:54:44 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,14 @@ function App() {
   const [newTodo, setNewTodo] = useState<string>("")
   const [todos, setTodos] = useState<Todo>({})
 
+  const noShadow: Object = {
+		boxShadow: "none",
+		mozBoxShadow: "none",
+		webkitBoxShadow: "none",
+		color: "red",
+	}
+
+
   const toggleTheme = (): void => {
     if (theme === "lightTheme") {
       setTheme("darkTheme")
@@ -33,69 +41,79 @@ function App() {
     setTodos(newTodos)
   }
 
+  const toggleTodo = (task: string) => {
+    const newTodos = { ...todos }
+    newTodos[task] = !newTodos[task]
+    setTodos(newTodos)
+  }
+
   return (
-    <div className={`app ${theme}`}>
-      <div className="images">
-        <img
-          style={theme == "lightTheme" ? { opacity: 1 } : { opacity: 0 }}
-          src="/bg-desktop-light.jpg"
-          alt=""
-        />
-        <img
-          style={theme == "lightTheme" ? { opacity: 0 } : { opacity: 1 }}
-          src="/bg-desktop-dark.jpg"
-          alt=""
-        />
-      </div>
-      <div className="background"></div>
-      <div className="container">
-        <div className="header">
-          <h1>TODO</h1>
-          <div className="icons">
-            <button
-              style={theme == "lightTheme" ? { opacity: 1 } : { opacity: 0 }}
-              onClick={toggleTheme}
-            >
-              <img src="/icon-moon.svg" alt="" />
-            </button>
-            <button
-              style={theme == "lightTheme" ? { opacity: 0 } : { opacity: 1 }}
-              onClick={toggleTheme}
-            >
-              <img src="/icon-sun.svg" alt="" />
-            </button>
-          </div>
-        </div>
-        <div className="newTodo">
-          <div className="checkBox"></div>
-          <input
-            className="input"
-            placeholder="Create a new todo..."
-            type="text"
-            value={newTodo}
-            onChange={(e: any) => {
-              setNewTodo(e.target.value)
-            }}
-            onKeyDown={handleKeyDown}
-          />
-        </div>
-        <div className="todosContainer">
-          <div className="todos">
-            {Object.keys(todos).map((todo: string, index) => {
-              return (
-                <Todo
-                  id={index}
-                  completed={todos[todo]}
-                  text={todo}
-                  onClick={deleteTodo}
-                />
-              )
-            })}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+		<div className={`app ${theme}`}>
+			<div className="images">
+				<img
+					style={theme == "lightTheme" ? { opacity: 1 } : { opacity: 0 }}
+					src="/bg-desktop-light.jpg"
+					alt=""
+				/>
+				<img
+					style={theme == "lightTheme" ? { opacity: 0 } : { opacity: 1 }}
+					src="/bg-desktop-dark.jpg"
+					alt=""
+				/>
+			</div>
+			<div className="background"></div>
+			<div className="container">
+				<div className="header">
+					<h1>TODO</h1>
+					<div className="icons">
+						<button
+							style={theme == "lightTheme" ? { opacity: 1 } : { opacity: 0 }}
+							onClick={toggleTheme}
+						>
+							<img src="/icon-moon.svg" alt="" />
+						</button>
+						<button
+							style={theme == "lightTheme" ? { opacity: 0 } : { opacity: 1 }}
+							onClick={toggleTheme}
+						>
+							<img src="/icon-sun.svg" alt="" />
+						</button>
+					</div>
+				</div>
+				<div className="newTodo">
+					<div className="checkBox"></div>
+					<input
+						className="input"
+						placeholder="Create a new todo..."
+						type="text"
+						value={newTodo}
+						onChange={(e: any) => {
+							setNewTodo(e.target.value)
+						}}
+						onKeyDown={handleKeyDown}
+					/>
+				</div>
+				<div
+					className="todosContainer"
+					style={Object.keys(todos).length == 0 ? noShadow : {}}
+				>
+					<div className="todos">
+						{Object.keys(todos).map((todo: string, index) => {
+							return (
+								<Todo
+									id={index}
+									completed={todos[todo]}
+									text={todo}
+									onCrossClick={deleteTodo}
+                  handleCompleted={toggleTodo}
+								/>
+							)
+						})}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,33 +1,40 @@
 interface TodoProps {
-  id: number
-  completed: boolean
-  text: string
-  onClick: (task: string) => void
+	id: number
+	completed: boolean
+	text: string
+	onCrossClick: (task: string) => void
+	handleCompleted: (task: string) => void
 }
 
-const Todo = ({ id, completed, text, onClick }: TodoProps) => {
-  return (
-    <div key={id} className="todo">
-      <div className="todoCheckBox">
-        <div
-          style={completed ? { opacity: 0 } : { opacity: 1 }}
-          className="todoCheckBoxInnerCircle"
-        >
-          <img
-            style={completed ? { opacity: 1 } : { opacity: 0 }}
-            src="/icon-check.svg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div className="todoText">
-        <span>{text}</span>
-      </div>
-      <button className="cross" onClick={() => onClick(text)}>
-        <img src="/icon-cross.svg" alt="" />
-      </button>
-    </div>
-  )
+const Todo = ({
+	id,
+	completed,
+	text,
+	onCrossClick,
+	handleCompleted,
+}: TodoProps) => {
+	return (
+		<div key={id} className="todo" onClick={() => handleCompleted(text)}>
+			<div className="todoCheckBox">
+				<div
+					style={completed ? { opacity: 0 } : { opacity: 1 }}
+					className="todoCheckBoxInnerCircle"
+				>
+					<img
+						style={completed ? { opacity: 1 } : { opacity: 0 }}
+						src="/icon-check.svg"
+						alt=""
+					/>
+				</div>
+			</div>
+			<div className="todoText">
+				<span>{text}</span>
+			</div>
+			<button className="cross" onClick={() => onCrossClick(text)}>
+				<img src="/icon-cross.svg" alt="" />
+			</button>
+		</div>
+	)
 }
 
 export default Todo
